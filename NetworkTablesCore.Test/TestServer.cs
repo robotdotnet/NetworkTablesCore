23bbf9226df33124978acbefaa66cdfbe575058b
Thad House <ThadHouse@users.noreply.github.com>
2015-11-03 10:26:35 -0800

    Debugging Test Server

--- NetworkTablesCore.Test/TestServer.cs
+++ NetworkTablesCore.Test/TestServer.cs
@@ -6,33 +6,46 @@ using NUnit.Framework;
 
 namespace NetworkTablesCore.Test
 {
+    [TestFixture]
     public class TestServer
     {
+        [Test]
         public void Test()
         {
             NetworkTable.Shutdown();
-
+            Console.WriteLine("Shutting Down");
             CoreMethods.SetLogger(((level, file, line, message) =>
             {
-                Console.Error.WriteLine(message);
+                //Console.Error.WriteLine(message);
             }), 0);
 
             NetworkTable.SetIPAddress("127.0.0.1");
+            Console.WriteLine("IP");
             NetworkTable.SetPort(10000);
+            Console.WriteLine("Port");
             NetworkTable.SetServerMode();
+            Console.WriteLine("Server");
             NetworkTable nt = NetworkTable.GetTable("");
+            Console.WriteLine("GetTable");
 
             Thread.Sleep(1000);
+            Console.WriteLine("FirstSleep");
 
             nt.PutNumber("foo", 0.5);
+            Console.WriteLine("foo .5");
             nt.SetFlags("foo", EntryFlags.Persistent);
-
+            Console.WriteLine("Persistent");
             nt.PutNumber("foo2", 0.5);
+            Console.WriteLine("Foo 2 .5");
             nt.PutNumber("foo2", 0.7);
+            Console.WriteLine("Foo 2 .7");
             nt.PutNumber("foo2", 0.6);
+            Console.WriteLine("Foo 2 .6");
             nt.PutNumber("foo2", 0.5);
+            Console.WriteLine("Foo 2 .5 2");
 
             Thread.Sleep(1000);
+            Console.WriteLine("2ndSleep");
         }
     }
 }
